ed8c88ce98f4f2ae6e3b37ed3878f98796ea1daf
pradeep <pradeepraghav@gmail.com>
2023-04-04 21:42:03 +0530

    pandas completed

--- pandas_test/pandascomplete.py
+++ pandas_test/pandascomplete.py
@@ -10,6 +10,8 @@ df=pd.DataFrame(dict1)
 # df.to_csv("friends1.csv",index=False)
 df.to_csv("friends1.csv")
 df.index=['first','second']
+
+
 # print(df)
 #
 # print (pd.read_csv('friend1.csv'))
@@ -22,3 +24,39 @@ df.index=['first','second']
 # fri['name'][0]="manish"
 
 # fri.to_csv('friend1.csv')
+
+newdf=pd.DataFrame(np.random.rand(334,5),index=np.arange(334))
+# print(newdf)
+# print(newdf.head())
+# print(type(newdf))
+# print(newdf.describe())
+# print (newdf.index)
+# print(newdf.columns)
+# print(newdf.T)
+
+print(newdf[0])
+newdf2=newdf # view
+newdf2=newdf.copy()
+newdf.drop(0,axis=1)
+print(newdf2)
+#newdf.loc([0,1],[0,1])
+
+# print(newdf)
+# print(newdf.iloc[0,4])
+#print(newdf.iloc[[0,1],[1,1]])
+print(newdf.head())
+print(newdf.drop(0))
+print(newdf.head())
+
+
+
+
+newdf.drop(4)
+newdf[1]=None
+print(newdf.dropna())
+
+newdf[0]=None
+# print(newdf)
+# print(newdf.fillna(120))
+print(newdf.duplicated())
+print(newdf.drop_duplicates())
